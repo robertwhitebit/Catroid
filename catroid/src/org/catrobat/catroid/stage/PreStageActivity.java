d32f8daaa0f3b201e269a0b49a57e051e044bb9e
Johannes Kuehnel <johannes.kuehnel@student.tugraz.at>
2014-10-09 10:38:33 +0200

    rework of WhenNfcBrick
    fixed test targets
    
    fixed hardware and software tests to reflect correct tag ids
    
    fixed back-button behavior from NfcTagFragment to ScriptFragment
    
    code refactoring
    
    fixed displaying of NFC Tag menu in the program menu on first run
    
    disabled NFC per default
    
    changed tests to reflect NFC being disabled per default
    
    fixed test case
    
    removed unused import
    
    fixed fragment tests
    
    fixed pmd & lint warnings
    
    changed tearDown regarding soundfile deletion
    
    removed hardware tests due to Arduino problems
    
    added check for enabling NFC in NfcTagFragment
    
    added NFC to permissions.txt
    
    added missing import
    
    fixed imports
    
    fixed checkstyle warning - imports, toasts, copyright

--- catroid/src/org/catrobat/catroid/stage/PreStageActivity.java
+++ catroid/src/org/catrobat/catroid/stage/PreStageActivity.java
@@ -23,6 +23,7 @@
 package org.catrobat.catroid.stage;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -30,10 +31,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.speech.tts.TextToSpeech.OnUtteranceCompletedListener;
@@ -65,6 +69,7 @@ import java.util.Locale;
 public class PreStageActivity extends BaseActivity {
 
 	private static final String TAG = PreStageActivity.class.getSimpleName();
+	//public static final int REQUEST_NFC_ADAPTER = 10000;
 	private static final int REQUEST_ENABLE_BLUETOOTH = 2000;
 	private static final int REQUEST_CONNECT_DEVICE = 1000;
 	public static final int REQUEST_RESOURCES_INIT = 101;
@@ -163,9 +168,33 @@ public class PreStageActivity extends BaseActivity {
 			}
 		}
 
+		if ((requiredResources & Brick.NFC_ADAPTER) > 0) {
+			NfcAdapter adapter = NfcAdapter.getDefaultAdapter(getApplicationContext());
+			if (adapter != null && !adapter.isEnabled())
+			{
+				ToastUtil.showError(PreStageActivity.this, R.string.nfc_not_activated);
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+					Intent intent = getNfcSettingsIntentForJellyBean();
+					startActivity(intent);
+				} else {
+					Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+					startActivity(intent);
+				}
+			} else if (adapter == null) {
+				ToastUtil.showError(PreStageActivity.this, R.string.no_nfc_available);
+				// TODO: resourceFailed() & startActivityForResult(), if behaviour needed
+			}
+			resourceInitialized();
+		}
+
 		if (requiredResourceCounter == Brick.NO_RESOURCES) {
 			startStage();
 		}
+	}
+
+	@TargetApi(16)
+	private Intent getNfcSettingsIntentForJellyBean() {
+		return new Intent(Settings.ACTION_NFC_SETTINGS);
 	}
 
 	public DroneInitializer getDroneInitializer() {
